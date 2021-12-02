9ad1acfe4c4b03fa483d6884bf9fb1e9ecb36e35
Stefano Romanò <rumix87@gmail.com>
2021-12-02 22:10:29 +0100

    Added base version of the load Injector

---
+++ injector/injector.go
@@ -0,0 +1,107 @@
+package injector
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/rs/zerolog/pkgerrors"
+	"harkonnen/shooter"
+	"io"
+	"sync"
+	"time"
+)
+
+type Injector struct {
+	Context context.Context
+	Logger  zerolog.Logger
+
+	SetUpScript    shooter.Script
+	MainScripts    []shooter.Script
+	TearDownScript shooter.Script
+	loadProfiles   []LoadProfile
+
+	shooters  []shooter.Shooter
+	waitGroup sync.WaitGroup
+
+	cancelFunc context.CancelFunc
+}
+
+func New(ctx context.Context, logWriter io.Writer) *Injector {
+	output := new(Injector)
+	output.Context, output.cancelFunc = context.WithCancel(ctx)
+
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	output.Logger = zerolog.New(logWriter).With().Timestamp().Logger()
+
+	return output
+}
+
+func (i *Injector) AddLoadProfile(profile LoadProfile) {
+	i.loadProfiles = append(i.loadProfiles, profile)
+}
+
+func (i *Injector) ExpectedShooters(elapsed time.Duration) int {
+	expected := 0
+
+	for _, ramp := range i.loadProfiles {
+		expected += ramp.At(elapsed)
+	}
+
+	return expected
+}
+
+func (i *Injector) AdjustScheduling(elapsed time.Duration) error {
+	expectedShooters := i.ExpectedShooters(elapsed)
+	activeShooters := i.ActiveShooters()
+
+	var err error
+
+	if activeShooters < expectedShooters {
+		err = i.addShooter()
+	}
+
+	if activeShooters > expectedShooters {
+		err = i.removeShooter()
+	}
+
+	return err
+}
+
+func (i *Injector) ActiveShooters() int {
+	return len(i.shooters)
+}
+
+func (i *Injector) addShooter() error {
+	newShooter := i.initShooter()
+	i.shooters = append(i.shooters, newShooter)
+	newShooter.Start()
+
+	return nil
+}
+
+func (i *Injector) removeShooter() error {
+	shooterToStop := i.shooters[0]
+	i.shooters = append(i.shooters[:0], i.shooters[1:]...)
+
+	shooterToStop.ScheduleShutDown()
+
+	return nil
+}
+
+func (i *Injector) initShooter() shooter.Shooter {
+	shooterID := uuid.NewString()
+	shooterLogger := log.With().Str("ID", shooterID).Logger()
+
+	newShooter := shooter.Shooter{
+		Context:        shooter.NewContext(i.Context, shooterLogger, shooterID),
+		SetUpScript:    i.SetUpScript,
+		MainScripts:    i.MainScripts,
+		TearDownScript: i.TearDownScript,
+		MaxIterations:  0,
+		WaitGroup:      &i.waitGroup,
+	}
+
+	return newShooter
+}
